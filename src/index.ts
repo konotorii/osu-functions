1f91d8f9dc55781f95f31ed1c8545e62c748fd3d
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-02-25 19:42:42 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-export * as scoresRead from './utility/src/scoresRead';
-export * as scoresWrite from './utility/src/scoresWrite';
-export * as collectionRead from './utility/src/collectionRead';
-export * as collectionWrite from './utility/src/collectionWrite';
-export * as osuDbRead from './utility/src/osuDbRead';
-export * as osuDbWrite from './utility/src/osuDbWrite';
-export * as beatmapRead from './utility/src/beatmapRead';
-export * as beatmapWrite from './utility/src/beatmapWrite';
-export * as replayRead from './utility/src/replayRead';
-export * as replayWrite from './utility/src/replayWrite';
+export * as tools from './utility'
